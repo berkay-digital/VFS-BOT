c6a2ffd6ed4707256ed8d58cc67c0e7c6477955e
berkay-digital <46861579+berkay-digital@users.noreply.github.com>
2023-10-18 22:28:30 +0200

    Add visa type selector

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from selenium.webdriver import Chrome                               # Chromedriver
-from selenium.webdriver.common.by import By                         # To find the elements
-from selenium.webdriver.support.ui import WebDriverWait             # To wait
-from selenium.webdriver.support import expected_conditions as EC    # To wait
-from selenium.webdriver.chrome.options import Options               # To resize the chrome window
-from selenium.common.exceptions import TimeoutException             # To apply try/except
+from selenium.webdriver import Chrome                       
+from selenium.webdriver.common.by import By                        
+from selenium.webdriver.support.ui import WebDriverWait           
+from selenium.webdriver.support import expected_conditions as EC   
+from selenium.webdriver.chrome.options import Options            
+from selenium.common.exceptions import TimeoutException            
 import time
 from vlc import MediaPlayer
 name = ""
@@ -34,7 +34,7 @@ else:
 driver = Chrome()
 
 driver.get("https://visa.vfsglobal.com/tur/pl/pol/login")
-time.sleep(2)
+time.sleep(5)
 try:
     WebDriverWait(driver, 10) \
                 .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
@@ -52,33 +52,40 @@ input("Press enter to proceed after entering the credentials.")
 
 
 def search():
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-checkbox-1"))).click()
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-checkbox-2"))).click()
-    time.sleep(2)
-    driver.set_window_size(100, 700)    
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-raised-button.mat-button-base"))).click()    
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-checkbox-3"))).click()
-    time.sleep(2)
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-stroked-button.mat-button-base"))).click()    
-    time.sleep(2)
-    driver.maximize_window()
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-select-0"))).click()
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//mat-option/span[contains(., 'Male')]"))).click()
-    time.sleep(2)
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-select-2"))).click()
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//mat-option/span[contains(., 'TURKEY')]"))).click()
-    time.sleep(2)
-    driver.find_element(By.ID, "mat-input-3").send_keys(name)
-    driver.find_element(By.ID, "mat-input-4").send_keys(surname)
-    driver.find_element(By.ID, "mat-input-5").send_keys(passport)
-    driver.find_element(By.ID, "mat-input-6").send_keys(code)
-    driver.find_element(By.ID, "mat-input-7").send_keys(number)
-    driver.find_element(By.ID, "mat-input-8").send_keys(mail)
-    time.sleep(30)
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.mat-focus-indicator.mat-stroked-button.mat-button-base.btn.btn-block.btn-brand-orange.mat-btn-lg"))).click()
-    # Let's test it until here.
-
-
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-checkbox-1"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-checkbox-2"))).click()
+        time.sleep(2)
+        driver.set_window_size(100, 700)    
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-raised-button.mat-button-base"))).click()    
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-checkbox-3"))).click()
+        time.sleep(2)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-stroked-button.mat-button-base"))).click()    
+        time.sleep(2)
+        driver.maximize_window()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-select-0"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//mat-option/span[contains(., 'Male')]"))).click()
+        time.sleep(2)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-select-2"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//mat-option/span[contains(., 'TURKEY')]"))).click()
+        time.sleep(2)
+        driver.find_element(By.ID, "mat-input-3").send_keys(name)
+        driver.find_element(By.ID, "mat-input-4").send_keys(surname)
+        driver.find_element(By.ID, "mat-input-5").send_keys(passport)
+        driver.find_element(By.ID, "mat-input-6").send_keys(code)
+        driver.find_element(By.ID, "mat-input-7").send_keys(number)
+        driver.find_element(By.ID, "mat-input-8").send_keys(mail)
+        time.sleep(30)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.mat-focus-indicator.mat-stroked-button.mat-button-base.btn.btn-block.btn-brand-orange.mat-btn-lg"))).click()
+        # Let's test it until here. Edit: it works
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-select-3"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//mat-option/span[contains(., '1 - Wiza typu D')]"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,"mat-select-4"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//mat-option/span[contains(., '4 - wiza typu D w celu innym  niż wymienione')]"))).click()
+        time.sleep(5)
+    except TimeoutException:
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "navbarDropdown"))).click()
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "dropdown-item"))).click()
 while True:
     search()
     time.sleep(960)
